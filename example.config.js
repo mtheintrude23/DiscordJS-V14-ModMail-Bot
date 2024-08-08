d64a0724bb447fa2a37143e11d6e71b20fb0e0ab
mtheintrude23 <167693071+mtheintrude23@users.noreply.github.com>
2024-08-08 08:53:54 +0700

    Update example.config.js

--- example.config.js
+++ example.config.js
@@ -6,7 +6,7 @@ module.exports = {
     modmail: {
         guildId: '', // ← Your server ID
         categoryId: '', // ← The modmail category ID
-        staffRoles: [''], // ← The modmail staff roles IDs
+        staffRoles: ['<@&1242778228561809500>'], // ← The modmail staff roles IDs
         mentionStaffRolesOnNewMail: true // ← Mention staff roles when there is a new mail?
     },
     logs: {
